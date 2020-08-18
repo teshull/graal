73442bab55b8aca68ec402b11428d6cd7e388775
Swapnil Gaikwad <swapnil.gaikwad@arm.com>
2020-08-18 09:06:41 +0100

    Add intrinsics for unsafe atomics for byte/short on AArch64
    Add intrinsic support for byte/short methods in unsafe API.
    Update the tests for Int/Long atomic intrinsics to reflect their
    existing support. Also, update the AtomicReadAndAddOp for AMD64 to
    preserve the sign while operating on byte/word size operands.
    
    This patch improves performance of Unsafe.getAndSet/Add on byte/short by
    about 27.6% on A72 machine.
    
    Change-Id: Ib9735fa47b3cfd69275a2170e03ec96a6a87b1ab

--- compiler/src/org.graalvm.compiler.core.aarch64/src/org/graalvm/compiler/core/aarch64/AArch64LIRGenerator.java
+++ compiler/src/org.graalvm.compiler.core.aarch64/src/org/graalvm/compiler/core/aarch64/AArch64LIRGenerator.java
@@ -152,8 +152,10 @@ public abstract class AArch64LIRGenerator extends LIRGenerator {
     }
 
     @Override
-    public Variable emitLogicCompareAndSwap(LIRKind accessKind, Value address, Value expectedValue, Value newValue, Value trueValue, Value falseValue, boolean useBarriers) {
-        emitCompareAndSwap(address, expectedValue, newValue, useBarriers);
+    public Variable emitLogicCompareAndSwap(LIRKind accessKind, Value address, Value expectedValue, Value newValue, Value trueValue, Value falseValue) {
+        Variable prevValue = newVariable(expectedValue.getValueKind());
+        Variable scratch = newVariable(LIRKind.value(AArch64Kind.DWORD));
+        append(new CompareAndSwapOp((AArch64Kind) accessKind.getPlatformKind(), loadReg(prevValue), expectedValue, loadReg(newValue), asAllocatable(address), scratch));
         assert trueValue.getValueKind().equals(falseValue.getValueKind());
         assert isIntConstant(trueValue, 1) && isIntConstant(falseValue, 0);
         Variable result = newVariable(trueValue.getValueKind());
@@ -162,14 +164,10 @@ public abstract class AArch64LIRGenerator extends LIRGenerator {
     }
 
     @Override
-    public Variable emitValueCompareAndSwap(LIRKind accessKind, Value address, Value expectedValue, Value newValue, boolean useBarriers) {
-        return emitCompareAndSwap(address, expectedValue, newValue, useBarriers);
-    }
-
-    private Variable emitCompareAndSwap(Value address, Value expectedValue, Value newValue, boolean useBarriers) {
-        Variable result = newVariable(expectedValue.getValueKind());
-        Variable scratch = newVariable(LIRKind.value(AArch64Kind.DWORD));
-        append(new CompareAndSwapOp(result, loadReg(expectedValue), loadReg(newValue), asAllocatable(address), scratch, useBarriers));
+    public Variable emitValueCompareAndSwap(LIRKind accessKind, Value address, Value expectedValue, Value newValue) {
+        Variable result = newVariable(newValue.getValueKind());
+        Variable scratch = newVariable(LIRKind.value(AArch64Kind.WORD));
+        append(new CompareAndSwapOp((AArch64Kind) accessKind.getPlatformKind(), loadReg(result), loadReg(expectedValue), loadReg(newValue), asAllocatable(address), scratch));
         return result;
     }
 
